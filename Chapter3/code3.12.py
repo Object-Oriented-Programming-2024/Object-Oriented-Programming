293feed8a803b3927e60493fdb418061f2efcf52
Shahbano <shahbano_farooq@hotmail.com>
2024-06-24 11:08:53 +0400

    Updating Chapter 3 code

--- Chapter3/code3.12.py
+++ Chapter3/code3.12.py
@@ -1,9 +1,9 @@
 # Set age limit for a job application
 try:
- age = int(input("Enter your age: "))
- assert (age > 20 and age < 60), age
+ age = int(input("Enter your age: ")) # Get age
+ assert (age > 20 and age < 60), age  # Check if age is between 20 and 60
  print("You can apply for the job!")
-except AssertionError as ex:
+except AssertionError as ex: # Catches the error raised if the age is not between 20 and 60
  print("Age", ex, "is not suitable for this job.")
-except:
+except: # Catches all errors
  print("Something went wrong.")
