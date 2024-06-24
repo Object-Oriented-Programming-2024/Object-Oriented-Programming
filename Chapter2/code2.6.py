f3c4d3203c77a4750d4764121362940d83aaa6c7
Shahbano <shahbano_farooq@hotmail.com>
2024-06-24 11:00:21 +0400

    Updating Chapter 2 code

--- Chapter2/code2.6.py
+++ Chapter2/code2.6.py
@@ -1,5 +1,5 @@
 class Customer: # Create a class customer with its attributes
- def __init__(self, first_name, last_name, email, transactions):
+ def __init__(self, first_name, last_name, email, transactions): # initialize a customer
      self.first_name = first_name
      self.last_name = last_name
      self.email = email
